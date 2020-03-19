d85d90d840fdc35d6fcd1d5c8f8fb7bdbd2b7fe4
Ubuntu <ubuntu@ip-172-31-0-64.ap-southeast-2.compute.internal>
2020-03-19 13:41:21 +0000

    user controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+Use App\User;
 use Illuminate\Http\Request;
 
 /*
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('users/{id}', 'UserController@show');
